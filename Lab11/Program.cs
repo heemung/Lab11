7d591403ff11caa5e238bbbdd0bb42f1f5d4cc0b
Clayton <clayton.cox@gmail.com>
2018-10-24 09:14:08 -0400

    adding the cat search

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -11,44 +11,48 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
-            /*
-            ArrayList allMovies = new ArrayList{new Movies("The Maxtrix", "scifi"),
-                new Movies("Big Mouth The Movie", "animated"),
-                new Movies("Another Teen Movie", "drama"),
-                new Movies("Wreck it Ralph", "animated"),
-                new Movies("The Village", "horror"),
-                new Movies("Star Wars", "scifi"),
-                new Movies("Finding Nemo", "animated"),
-                new Movies("Halloween", "horror"),
-                new Movies("The Love Boat", "drama"),
-                new Movies("Another Teen Movie", "drama")};
-            Movies movieList = new Movies("The Maxtrix", "scifi");
-            */
-
-
-            /*
-            foreach(Movies m in theseArrays)
-            {
-                m.TheCatergoryCode = theseArrays.IndexOf(m) + 1;
-                Console.WriteLine(m.TheCatergoryCode);
-            }*/
             string userInputCat;
-            Console.WriteLine("Please enter a category");
-            userInputCat = Console.ReadLine();
+            Movies mRef = new Movies("temp", "temp");
+            ArrayList allMovies = AddingMovies();
 
-            foreach (Movies m in allMovies)
+            Console.WriteLine("Please enter a category");
+            // bool newbool = (mRef.TheMovieCategory)allMovies.Contains(userInputCat.);
+            //bool newBool = allMovies.Contains(mRef.TheMovieCategory == userInputCat);
+            userInputCat = Console.ReadLine();
+            mRef.TheMovieCategory = userInputCat;
+            bool newBool = allMovies.Contains(mRef.TheMovieCategory);
+
+            if (allMovies.Contains(mRef.TheMovieCategory))
+            {
+                foreach (Movies m in allMovies)
+                {
+                    if (m.TheMovieCategory == userInputCat)
+                    {
+                        Console.WriteLine(m.TheMovieName);
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("not avaible");
+            }
+
+/*
+            foreach (Movies m in AddingMovies())
             {
                 if(m.TheMovieCategory == userInputCat)
                 {
                     Console.WriteLine(m.TheMovieName);
                 }
             }
-            
+            */
 
            Console.ReadLine();
     
         }
-        public ArrayList AddingMovies()
+
+        public static ArrayList AddingMovies()
         {
                 ArrayList allMovies = new ArrayList{new Movies("The Maxtrix", "scifi"),
                 new Movies("Big Mouth The Movie", "animated"),
